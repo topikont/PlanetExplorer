3d359fae26af279df0eae327501ad74d9c3d359f
somename <someemail>
2015-10-22 13:29:03 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -89,7 +89,9 @@ public class PlanetExplorer {
 				String obstacleString = checkCollision();
 				
 				if(obstacleString != null) {
-					encounteredObstacles += obstacleString;
+					if() {
+						encounteredObstacles += obstacleString;
+					}
 					moveExplorer("f");
 				}
 				
